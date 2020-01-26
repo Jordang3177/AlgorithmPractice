5283219389810b63fd0c39d278e8e3718a08ffef
Jordan Graves <Jordang3177@gmail.com>
2020-01-26 21:29:36 +0800

    Finished up strStr with improvements to make it faster."

--- Python/strStr.py
+++ Python/strStr.py
@@ -7,9 +7,19 @@ class Solution:
                 return 0
         if len(needle) == 0:
             return 0
-        answer = 0
-        ended = False
-        return 0
+        for i in range(0, len(haystack)):
+            if haystack[i] == needle[0]:
+                answer = i
+                if len(needle) == 1:
+                    return answer
+                if len(haystack[i:]) < len(needle):
+                    return -1
+                if needle in haystack[i:len(needle) + i]:
+                    return answer
+        return -1
+
+
+
 
 
     def teststrStr(self):
@@ -21,6 +31,8 @@ class Solution:
         assert self.strStr('Wow', 'wowoww') == -1
         assert self.strStr('Same', 'Same') == 0
         assert self.strStr('wow', '') == 0
+        assert self.strStr('mississippi', 'issipi') == -1
+        assert self.strStr('mississippi', 'issip') == 4
 
 if __name__ == '__main__':
     S = Solution()
