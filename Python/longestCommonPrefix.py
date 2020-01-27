9e53903f73d90419cde3cef3e10daada74738366
Jordan Graves <Jordang3177@gmail.com>
2020-01-27 19:31:51 +0800

    Work in progress

--- Python/longestCommonPrefix.py
+++ Python/longestCommonPrefix.py
@@ -1,6 +1,17 @@
 class Solution:
     def longestCommonPrefix(self, strs):
-        print('work in Progrees')
+        if len(strs) == 0:
+            return ''
+        counter = 0
+        answer = ''
+        previous = False
+        for char in strs[0]:
+            for str in strs:
+                if char in str:
+                    counter += 1
+                else:
+                    print('work in progress')
+
 
     def testLongestCommonPrefix(self):
         assert self.longestCommonPrefix(['flower', 'flow', 'flight']) == 'fl'
@@ -9,6 +20,8 @@ class Solution:
         assert self.longestCommonPrefix(['wow', 'wow', 'wow', 'wow', 'wow']) == 'wow'
         assert self.longestCommonPrefix(['lol', 'lol', 'lol', 'lol', 'ololol', 'n']) == ''
         assert self.longestCommonPrefix(['okthen', 'lol', 'wow', 'only']) == 'o'
+        assert self.longestCommonPrefix([]) == ''
+        
 
 if __name__ == '__main__':
     S = Solution()
