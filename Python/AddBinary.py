cdcaf360233bb837a1c9d0e31a5ad578de895533
Jordan Graves <Jordang3177@gmail.com>
2020-02-03 20:19:11 +0800

    Started AddBinary

---
+++ Python/AddBinary.py
@@ -0,0 +1,13 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        return 'wow'
+
+    def testAddBinary(self):
+        assert self.addBinary('11', '1') == '100'
+        assert self.addBinary('1010', '1011') == '10101'
+
+
+if __name__ == '__main__':
+    S = Solution()
+    S.testAddBinary()
+    print("All Tests Passed")
