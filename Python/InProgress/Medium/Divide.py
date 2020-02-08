cfa827f82c17fbfed660bfebb02cb49a6db47d26
Jordan Graves <Jordang3177@gmail.com>
2020-02-08 22:46:37 +0800

    Work in Progress

--- Python/InProgress/Medium/Divide.py
+++ Python/InProgress/Medium/Divide.py
@@ -1,14 +1,34 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        if divisor == 1:
-            return dividend
-        if divisor == -1:
-            return -dividend
-        return 0
+        answer = 0
+        if divisor > 0 and dividend > 0:
+            while dividend >= divisor:
+                dividend = dividend - divisor
+                answer += 1
+        if divisor < 0 and dividend > 0:
+            while dividend >= abs(divisor):
+                dividend = dividend + divisor
+                answer -= 1
+        if divisor < 0 and dividend < 0:
+            while abs(dividend) >= abs(divisor):
+                dividend = dividend - divisor
+                answer += 1
+        if divisor > 0 and dividend < 0:
+            while abs(dividend) >= divisor:
+                dividend = dividend + divisor
+                answer -= 1
+        if answer > 2**31 - 1:
+            return 2**31 - 1
+        if answer < -2**31:
+            return -2**31
+        else:
+            return answer
 
     def testDivide(self):
         assert self.divide(10, 3) == 3
         assert self.divide(7, -3) == -2
+        assert self.divide(-4, 2) == -2
+        assert self.divide(-4, -2) == 2
 
 
 if __name__ == '__main__':
