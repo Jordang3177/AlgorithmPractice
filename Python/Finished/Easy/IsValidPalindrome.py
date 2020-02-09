6a6a319cf9714a7529733cda24fb273579d0a46e
Jordan Graves <Jordang3177@gmail.com>
2020-02-10 00:57:12 +0800

    Need to update later to make it faster

--- Python/Finished/Easy/IsValidPalindrome.py
+++ Python/Finished/Easy/IsValidPalindrome.py
@@ -1,6 +1,24 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        if len(s) < 2:
+            return True
+        i = 0
+        j = len(s) - 1
+        while i != j:
+            if i > j:
+                break
+            if s[i].isalpha() == False and s[i].isnumeric() == False:
+                i += 1
+                continue
+            if s[j].isalpha() == False and s[j].isnumeric() == False:
+                j -= 1
+                continue
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
         return True
+
 
     def testIsPalindrome(self):
         assert self.isPalindrome("A man, a plan, a canal: Panama") == True
@@ -10,6 +28,7 @@ class Solution:
         assert self.isPalindrome("") == True
         assert self.isPalindrome("Aa") == True
         assert self.isPalindrome("ab") == False
+        assert self.isPalindrome('0P') == False
 
 
 if __name__ == "__main__":
