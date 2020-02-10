e07aa02ef9efc85b930ff46f5e0cf8c79744d543
Jordan Graves <Jordang3177@gmail.com>
2020-02-11 00:51:01 +0800

    Faster than 62% and uses less memory than 30% of submissions

--- Python/Finished/Easy/IsAnagram.py
+++ Python/Finished/Easy/IsAnagram.py
@@ -2,13 +2,7 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        for i in range(0, len(s)):
-            if s[i] in t:
-                indext = t.index(s[i])
-                t = t[0:indext] + t[indext + 1:len(t)]
-            else:
-                return False
-        return True
+        
 
     def testIsAnagram(self):
         assert self.isAnagram('anagram', 'nagaram') == True
