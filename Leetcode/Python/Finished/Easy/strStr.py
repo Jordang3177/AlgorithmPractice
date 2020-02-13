6d7a74d66475865354f5054117928b8f67c1e3d9
Jordan Graves <Jordang3177@gmail.com>
2020-02-14 00:03:26 +0800

    Speed: 76%, Memory: 100%

--- Leetcode/Python/Finished/Easy/strStr.py
+++ Leetcode/Python/Finished/Easy/strStr.py
@@ -1,26 +1,9 @@
 class Solution:
     def strStr(self, haystack: str, needle: str):
-        if len(needle) > len(haystack):
+        if needle not in haystack:
             return -1
-        if len(needle) == len(haystack):
-            if needle == haystack:
-                return 0
-        if len(needle) == 0:
-            return 0
-        for i in range(0, len(haystack)):
-            if haystack[i] == needle[0]:
-                answer = i
-                if len(needle) == 1:
-                    return answer
-                if len(haystack[i:]) < len(needle):
-                    return -1
-                if needle in haystack[i:len(needle) + i]:
-                    return answer
-        return -1
-
-
-
-
+        else:
+            return haystack.index(needle)
 
     def teststrStr(self):
         assert self.strStr('hello', 'll') == 2
