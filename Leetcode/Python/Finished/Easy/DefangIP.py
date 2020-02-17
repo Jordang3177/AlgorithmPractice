f103ff710eb01bc74dab03b3355e19a5ecce4773
Jordan Graves <Jordang3177@gmail.com>
2020-02-17 18:27:28 +0800

    Added More to Unit Testing

--- Leetcode/Python/Finished/Easy/DefangIP.py
+++ Leetcode/Python/Finished/Easy/DefangIP.py
@@ -1,15 +1,3 @@
-def defangIPaddr(address):
-    address = address.replace('.', '[.]')
-    return address
-
-def defangIPaddr2(address):
-    newstring = ''
-    for i in range(len(address)):
-        if address[i] == '.':
-            newstring += '[.]'
-        else:
-            newstring += address[i]
-    return newstring
-
-print(defangIPaddr('1.1.1.1'))
-print(defangIPaddr2('1.1.1.1'))
+class Solution:
+    def defang_ip(self, str):
+        return str.replace('.', '[.]')
--- Leetcode/Python/Finished/Easy/test_finished_problems.py
+++ Leetcode/Python/Finished/Easy/test_finished_problems.py
@@ -3,7 +3,12 @@ from unittest import TestCase
 from Leetcode.Python.Finished.Easy.countAndSay import Solution as CountSolution
 from Leetcode.Python.Finished.Easy.AddBinary import Solution as BinarySolution
 from Leetcode.Python.Finished.Easy.atoi import Solution as AtoiSolution
-
+from Leetcode.Python.Finished.Easy.CheckIfExist import Solution as CIESolution
+from Leetcode.Python.Finished.Easy.ClimbingStairs import Solution as ClimbingSolution
+from Leetcode.Python.Finished.Easy.DefangIP import Solution as DefangSolution
+from Leetcode.Python.Finished.Easy.Fib import Solution as FibSolution
+from Leetcode.Python.Finished.Easy.IntegerToRoman import Solution as ITRSolution
+from Leetcode.Python.Finished.Easy.IsAnagram import Solution as AnagramSolution
 
 class TestSolution(TestCase):
     def test_count_and_say(self):
@@ -44,3 +49,67 @@ class TestSolution(TestCase):
         self.assertEqual(S.myAtoi('           0-1'), 0)
         self.assertEqual(S.myAtoi('0+1'), 0)
         self.assertEqual(S.myAtoi('              0+1'), 0)
+
+
+    def test_check_if_exist(self):
+        S = CIESolution()
+        self.assertEqual(S.checkIfExist([10,2,5,3]), True)
+        self.assertEqual(S.checkIfExist([1]), False)
+        self.assertEqual(S.checkIfExist([]), False)
+        self.assertEqual(S.checkIfExist([1,1]), False)
+        self.assertEqual(S.checkIfExist([1,3,5]), False)
+
+    def test_climbing_stairs(self):
+        S = ClimbingSolution()
+        self.assertEqual(S.climbStairs(0), 1)
+        self.assertEqual(S.climbStairs(1), 1)
+        self.assertEqual(S.climbStairs(2), 2)
+        self.assertEqual(S.climbStairs(3), 3)
+        self.assertEqual(S.climbStairs(4), 5)
+        self.assertEqual(S.climbStairs(5), 8)
+        self.assertEqual(S.climbStairs(6), 13)
+        self.assertEqual(S.climbStairs(7), 21)
+        self.assertEqual(S.climbStairs(8), 34)
+        self.assertEqual(S.climbStairs(9), 55)
+        self.assertEqual(S.climbStairs(10), 89)
+
+    def test_defang_ip(self):
+        S = DefangSolution()
+        self.assertEqual(S.defang_ip('1.1.1.1'), '1[.]1[.]1[.]1')
+        self.assertEqual(S.defang_ip('250.0.168.1'), '250[.]0[.]168[.]1')
+        self.assertEqual(S.defang_ip('168.1.1.0'), '168[.]1[.]1[.]0')
+
+    def test_fib(self):
+        S = FibSolution()
+        self.assertEqual(S.fib(0), 0)
+        self.assertEqual(S.fib(1), 1)
+        self.assertEqual(S.fib(2), 1)
+        self.assertEqual(S.fib(3), 2)
+        self.assertEqual(S.fib(4), 3)
+        self.assertEqual(S.fib(5), 5)
+        self.assertEqual(S.fib(6), 8)
+        self.assertEqual(S.fib(7), 13)
+        self.assertEqual(S.fib(8), 21)
+        self.assertEqual(S.fib(9), 34)
+        self.assertEqual(S.fib(10), 55)
+        self.assertEqual(S.fib(11), 89)
+        self.assertEqual(S.fib(300), 222232244629420445529739893461909967206666939096499764990979600)
+
+    def test_int_to_rom(self):
+        S = ITRSolution()
+        self.assertEqual(S.intToRoman(1), 'I')
+        self.assertEqual(S.intToRoman(4), 'IV')
+        self.assertEqual(S.intToRoman(1990), 'MCMXC')
+        self.assertEqual(S.intToRoman(3999), 'MMMCMXCIX')
+        self.assertEqual(S.intToRoman(90), 'XC')
+        self.assertEqual(S.intToRoman(500), 'D')
+        self.assertEqual(S.intToRoman(400), 'CD')
+        self.assertEqual(S.intToRoman(100), 'C')
+        self.assertEqual(S.intToRoman(50), 'L')
+        self.assertEqual(S.intToRoman(40), 'XL')
+        self.assertEqual(S.intToRoman(10), 'X')
+        self.assertEqual(S.intToRoman(5), 'V')
+
+    def test_is_anagram(self):
+        S = AnagramSolution()
+        self.assertEqual(S.isAnagram('anagram', 'nagaram'), True)
