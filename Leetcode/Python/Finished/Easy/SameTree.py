7a765dc2a5bea8a27febeee40c20ebda4bc74602
Jordan Graves <Jordang3177@gmail.com>
2020-02-26 16:30:08 +0800

    Speed: 90%, Memory: 100%

---
+++ Leetcode/Python/Finished/Easy/SameTree.py
@@ -0,0 +1,25 @@
+
+
+class Tree:
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.val = None
+
+
+class Solution:
+    def same_tree(self, p, q):
+        """
+        Checks if both given Treenodes are the same
+        :param p: TreeNode
+        :param q: TreeNode
+        :return: bool
+        """
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        else:
+            return self.same_tree(p.left, q.left) and self.same_tree(p.right, q.right)
