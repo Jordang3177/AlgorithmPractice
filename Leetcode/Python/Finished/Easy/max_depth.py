4211f6b85534deafe5e7839fd3120d507dbbd8ad
Jordan Graves <Jordang3177@gmail.com>
2020-02-26 18:51:20 +0800

    Speed: 88%, Mmeory: 100%

---
+++ Leetcode/Python/Finished/Easy/max_depth.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        answer = 1
+        if not root:
+            return 0
+        elif not root.left and not root.right:
+            return 1
+        elif not root.left:
+            answer += self.maxDepth(root.right)
+        elif not root.right:
+            answer += self.maxDepth(root.left)
+        else:
+            answer += max(self.maxDepth(root.left), self.maxDepth(root.right))
+        return answer
