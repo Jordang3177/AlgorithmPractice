8a14db8d719bf81ccd75ec4df78ecd791bb7b0e3
Jordan Graves <Jordang3177@gmail.com>
2020-02-27 11:38:02 +0800

    Speed: 99.95%, Memory: 100%

--- Leetcode/Python/Finished/Easy/isPalindrome.py
+++ Leetcode/Python/Finished/Easy/isPalindrome.py
@@ -5,45 +5,16 @@ class Solution:
         :param x: x
         :return: bool
         """
-        y = str(x)
-        if len(y) == 0:
-            return True
-        if len(y) == 1:
-            return True
-        if len(y) == 2:
-            return y[0] == y[1]
-        if y[0] == '-':
+        if x < 0:
             return False
-        if len(y) % 2:
-            first_half = y[0:int(len(y)/2)]
-            second_half = y[int(len(y)/2):len(y)]
-            i = 0
-            for num in first_half:
-                if num != second_half[len(second_half) - 1 - i]:
-                    return False
-                i += 1
-            return True
         else:
-            first_half = y[0:int(len(y)/2)]
-            second_half = y[int(len(y)/2) + 1: len(y)]
+            x = str(x)
             i = 0
-            for num in first_half:
-                if num != second_half[len(second_half) - 1 - i]:
+            j = len(x) - 1
+            while i <= j:
+                if x[i] != x[j]:
                     return False
-                i += 1
+                else:
+                    i += 1
+                    j -= 1
             return True
-
-    def test_isPalindrome(self):
-        assert self.isPalindrome(121) == True
-        assert self.isPalindrome(-121) == False
-        assert self.isPalindrome(122) == False
-        assert self.isPalindrome(1) == True
-        assert self.isPalindrome(0) == True
-        assert self.isPalindrome(111111111) == True
-        assert self.isPalindrome(1010101010101) == True
-        assert self.isPalindrome(10) == False
-
-if __name__ == "__main__":
-    S = Solution()
-    S.test_isPalindrome()
-    print("All Tests Passed")
