c66d4c5099ef6e998f85b6a416ef9ae405f2e4d4
Jordan Graves <Jordang3177@gmail.com>
2020-04-21 19:06:50 +0800

    Mahmou and Ehab and the even-odd game

---
+++ Codeforces/CPP/Finished/600/959A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        cout << "Mahmoud" << endl;
+    }
+    else
+    {
+        cout << "Ehab" << endl;
+    }
+    return 0;
+}
