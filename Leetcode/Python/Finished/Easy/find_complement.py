3c72e03fd348203c71724df00e499afd8cf9a4d1
Jordan Graves <Jordang3177@gmail.com>
2020-05-04 16:01:06 +0800

    Speed: 89%, Memory: 100%

--- Leetcode/Python/Finished/Easy/find_complement.py
+++ Leetcode/Python/Finished/Easy/find_complement.py
@@ -3,14 +3,12 @@ class Solution:
         answer = []
         while num > 0:
             if num % 2 == 1:
-                answer.append(1)
-            else:
                 answer.append(0)
+            else:
+                answer.append(1)
             num = num // 2
         complement = 0
         for i in range(len(answer)):
-            if answer[i] == 1:
-                complement += 0 * 2 ** i
-            else:
-                complement += 1 * 2 ** i
+            complement += answer[i] * 2 ** i
         return complement
+
