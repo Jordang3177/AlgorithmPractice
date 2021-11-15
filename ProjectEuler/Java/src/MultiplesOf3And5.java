b6a8b29f9ca1ecb2de668d663f232e4792533ead
Jordan Graves <Jordang3177@gmail.com>
2021-11-15 17:19:59 -0500

    Updated Git Ignore and Finished Problem 1

---
+++ ProjectEuler/Java/src/MultiplesOf3And5.java
@@ -0,0 +1,20 @@
+public class MultiplesOf3And5 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(MultiplesOf3And5(1000));
+    }
+
+    public static int MultiplesOf3And5(int limit) {
+        int sum = 0;
+        for(int i = 3; i < limit; i += 3) {
+            sum += i;
+        }
+        for(int i = 5; i < limit; i += 5) {
+            sum += i;
+        }
+        for(int i = 15; i < limit; i += 15) {
+            sum -= i;
+        }
+        return sum;
+    }
+}
