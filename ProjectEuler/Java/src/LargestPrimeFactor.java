74ab239d783019afa7e6bda9bf2636b951585cbc
Jordan Graves <Jordang3177@gmail.com>
2021-11-15 18:10:51 -0500

    Finished LargestPrimeFactor problem

--- ProjectEuler/Java/src/LargestPrimeFactor.java
+++ ProjectEuler/Java/src/LargestPrimeFactor.java
@@ -1,2 +1,22 @@
+import java.util.Stack;
+
 public class LargestPrimeFactor {
+    public static void main(String[] args) {
+        LargestPrimeFactor obj = new LargestPrimeFactor();
+        long answer = obj.function(600851475143L);
+        System.out.println(answer);
+    }
+
+    private Long function(long  x) {
+        long answer = 0;
+        for(int i = 2; i <= x; i++) {
+            if(x%i == 0) {
+                while(x%i == 0) {
+                    answer = i;
+                    x /= i;
+                }
+            }
+        }
+        return answer;
+    }
 }
